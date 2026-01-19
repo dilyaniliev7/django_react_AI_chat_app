376b1278613765247db615e99657433c933512d6
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-01-19 20:49:53 +0200

    additional django implementations

--- backend/promptify_app/views.py
+++ backend/promptify_app/views.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
-from promptify_app.models import Chat
-
+from rest_framework.decorators import api_view
+from promptify_app.models import Chat, ChatMessage
 
 client = OpenAI()
 
@@ -21,6 +22,7 @@ def createChatTitle(user_message):
         title = user_message[:50]
     return title
 
+@api_view(['POST'])
 def prompt_gpt(request):
     chat_id = request.data.get("chat_id")
     content = request.data.get("content")
@@ -34,3 +36,23 @@ def prompt_gpt(request):
     chat, created = Chat.object.get_or_create(id=chat_id)
     chat.title = createChatTitle(content)
     chat.save()
+
+    chat_message = ChatMessage.objects.create(role="user", chat=chat, content=content)
+
+    chat_messages = chat.messages.order_by("created_at")[:10]
+
+    openai_messages = [{"role": message.role, "content": message.content } for message in chat_messages]
+
+    if not any(message["role"]=="assistant" for message in openai_messages):
+        openai_messages.insert(0, {"role": "assistant", "content": "You are helpful assistant."})
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=openai_messages
+        )
+        openai_reply = response.choices[0].message.content
+    except Exception as e:
+        return Response({"error": f"An error from OpenAI {str(e)}"}, status=500)
+
+    ChatMessage.object.create(role="assistant", content=openai_reply, chat=chat)
+    return Response({"reply": openai_reply}, status=status.HTTP_201_CREATED)
